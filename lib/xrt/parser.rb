9e2f45232d44232aee561bd929d7a8196c811dc0
hitode909 <hitode909@gmail.com>
2016-11-21 22:29:14 +0900

    parse [%, %] manually

--- lib/xrt/parser.rb
+++ lib/xrt/parser.rb
@@ -5,7 +5,44 @@ module XRT
     end
 
     def statements
-      @source.split(/(\[%[^\]]+%\])/m)
+      reading = @source.clone
+      result = []
+
+      while reading.length > 0
+        got = read_block(reading) || read_text(reading)
+        unless got
+          raise "failed to parse #{@source}"
+        end
+        result << got
+      end
+
+      result
+    end
+
+    def read_block source
+      return nil unless source[0...2] == '[%'
+
+      buffer = ''
+      while source[0...2] != '%]'
+        buffer << source.slice!(0)
+      end
+      buffer << source.slice!(0, 2)
+      buffer
+    end
+
+    def read_text source
+      return nil if source[0...2] == '[%'
+
+      buffer = ''
+      while true
+        return buffer if source[0...2] == '[%'
+        break if source.length < 2
+        buffer << source.slice!(0)
+      end
+
+      buffer << source.slice!(0, source.length)
+
+      buffer
     end
   end
 end
--- test/test-xrt-parser.rb
+++ test/test-xrt-parser.rb
@@ -6,12 +6,33 @@ class TestXRTParser < Test::Unit::TestCase
     @parser = XRT::Parser.new
   end
 
+  def test_read_block
+    assert_equal '[% %]', @parser.read_block('[% %]')
+    assert_equal '[% [ ] %]', @parser.read_block('[% [ ] %]')
+    assert_nil @parser.read_block('hi')
+  end
+
+  def test_read_text
+    assert_equal 'hi', @parser.read_text('hi')
+    assert_equal 'hi[', @parser.read_text('hi[')
+    assert_equal 'hi', @parser.read_text('hi[%')
+    assert_nil @parser.read_text('[% %]')
+  end
+
   def test_statements
     test_cases = [
       ['<html>', ['<html>']],
       ['a [% b %] c', ['a ', '[% b %]', ' c']],
-      ['[% a %] [% b %] [% c %]', ['', '[% a %]', ' ', '[% b %]', ' ', '[% c %]']],
+      ['[% a %] [% b %] [% c %]', ['[% a %]', ' ', '[% b %]', ' ', '[% c %]']],
       ['[% FOR k IN [1, 2, 3] %]', ['[% FOR k IN [1, 2, 3] %]']],
+      [
+        %q([% WRAPPER "wrapper.tt" WITH args = [1,2,3] %]<div></div>[% END %]),
+        [
+          %q([% WRAPPER "wrapper.tt" WITH args = [1,2,3] %]),
+          %q(<div></div>),
+          %q([% END %]),
+        ]
+      ],
     ]
 
     test_cases.each{|test_case|
