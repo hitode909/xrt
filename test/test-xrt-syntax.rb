4362dc6c462150649e225ee93e154004c3e537fc
hitode909 <hitode909@gmail.com>
2016-11-21 22:33:31 +0900

    delete test

--- test/test-xrt-syntax.rb
+++ test/test-xrt-syntax.rb
@@ -10,11 +10,6 @@ class TC_XRT_Syntax < Test::Unit::TestCase
     assert_equal 'foo', @syntax.remove_comment('foo # bar')
   end
 
-  def test_beginning_block_regexp
-    assert_match @syntax.beginning_block_regexp, '[% IF 1 %]'
-    assert_match @syntax.beginning_block_regexp, '[% WHILE 1 %]'
-  end
-
   def test_beginning_block?
     assert @syntax.beginning_block? '[% IF 1 %]'
     assert @syntax.beginning_block? '[% WHILE 1 %]'
