484630b27030cd6e7d6186ee5b7b99b4c472168d
hitode909 <hitode909@gmail.com>
2016-11-22 14:35:13 +0900

    find_block, replace_block

---
+++ sketch/find_block.rb
@@ -0,0 +1,24 @@
+base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+lib_dir = File.join(base_dir, "lib")
+$LOAD_PATH.unshift(lib_dir)
+
+require 'xrt/parser'
+
+target_file, target_block = *ARGV
+
+warn "target_file: #{target_file}"
+warn "target_block: #{target_block}"
+
+source = open(target_file).read
+parser = XRT::Parser.new(source)
+doc = parser.document
+found = doc.find_blocks.select{|block|
+  block.content.index(target_block) == 0
+}.first
+
+if found
+  puts found.content
+else
+  warn 'not found'
+  exit 1
+end
