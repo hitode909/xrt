475d0cb6bb4b1da9f279a373845f3d5bba705718
hitode909 <hitode909@gmail.com>
2016-11-22 10:58:22 +0900

    making statement

---
+++ lib/xrt/statement.rb
@@ -0,0 +1,46 @@
+module XRT
+  class Statement
+    def initialize(content)
+      @content = content
+    end
+
+    def content
+      @content
+    end
+  end
+
+  class Statement
+    class Text < Statement
+    end
+
+    class Directive < Statement
+    end
+
+    class End < Directive
+    end
+
+    class Block < Directive
+      def initialize(content)
+        super
+        @children = []
+      end
+
+      def closed?
+        children.last.kind_of? End
+      end
+
+      def push_child statement
+        raise 'trying to push_child to closed block' if closed?
+        @children << statement
+      end
+
+      def children
+        @children
+      end
+
+      def end
+        nil
+      end
+    end
+  end
+end
---
+++ test/test-statement.rb
@@ -0,0 +1,52 @@
+require 'test/unit'
+require 'xrt/statement'
+
+class TestStatement < Test::Unit::TestCase
+  def test_text
+    text = XRT::Statement::Text.new('hi')
+    assert text.kind_of? XRT::Statement
+    assert_equal text.content, 'hi'
+  end
+
+  def test_directive
+    text = XRT::Statement::End.new('[% foo() %]')
+    assert text.kind_of? XRT::Statement
+    assert_equal text.content, '[% foo() %]'
+  end
+
+  def test_end
+    text = XRT::Statement::End.new('[% END %]')
+    assert text.kind_of? XRT::Statement
+    assert_equal text.content, '[% END %]'
+  end
+
+  def test_block
+    text = XRT::Statement::Block.new('[% IF 1 %]')
+    assert text.kind_of? XRT::Statement
+    assert_equal text.content, '[% IF 1 %]'
+    assert_equal text.children, []
+    assert_nil text.end
+  end
+end
+
+class TestBlock < Test::Unit::TestCase
+  def test_push_child
+    block = XRT::Statement::Block.new('[% IF 1 %]')
+    assert_equal false, block.closed?
+
+    statement_ok = XRT::Statement::Text.new('ok')
+    statement_end = XRT::Statement::End.new('[% END %]')
+
+    block.push_child statement_ok
+    assert_equal false, block.closed?
+
+    block.push_child statement_end
+    assert_equal true, block.closed?
+
+    assert_equal block.children, [statement_ok, statement_end]
+
+    assert_raises {
+      block.push_child statement_ok
+    }
+  end
+end
