0d181706496632febee4a1f8c04a69c65a8e195d
hitode909 <hitode909@gmail.com>
2016-12-13 20:59:00 +0900

    fix indent

--- test/test-parser.rb
+++ test/test-parser.rb
@@ -49,20 +49,20 @@ class TestParser < Test::Unit::TestCase
     ], doc.children
   end
 
-    def test_document_nested_block
-      parser = XRT::Parser.new('[% IF a %][% IF b %]1[% END %][% END %]')
-      doc = parser.document
-      assert doc.kind_of? XRT::Statement::Document
-      if_block1 = XRT::Statement::Block.new('[% IF a %]')
-      if_block2 = XRT::Statement::Block.new('[% IF b %]')
-      if_block2 << XRT::Statement::Text.new('1')
-      if_block2 << XRT::Statement::Text.new('[% END %]')
-      if_block1 << if_block2
-      if_block1 << XRT::Statement::Text.new('[% END %]')
-      assert_equal [
-        if_block1
-      ], doc.children
-    end
+  def test_document_nested_block
+    parser = XRT::Parser.new('[% IF a %][% IF b %]1[% END %][% END %]')
+    doc = parser.document
+    assert doc.kind_of? XRT::Statement::Document
+    if_block1 = XRT::Statement::Block.new('[% IF a %]')
+    if_block2 = XRT::Statement::Block.new('[% IF b %]')
+    if_block2 << XRT::Statement::Text.new('1')
+    if_block2 << XRT::Statement::Text.new('[% END %]')
+    if_block1 << if_block2
+    if_block1 << XRT::Statement::Text.new('[% END %]')
+    assert_equal [
+      if_block1
+    ], doc.children
+  end
 
   def test_read_directive
     assert_equal '[% %]', @parser.read_directive('[% %]')
